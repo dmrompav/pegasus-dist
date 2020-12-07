e0d66d64a873407b46212f3974535ad1df1539b3
dmrompav <dm.rom.pav@gmail.com>
2020-12-07 08:09:24 +0500

    img wtf

--- js/script.js
+++ js/script.js
@@ -6,10 +6,43 @@ window.onload = function () {
   document.querySelector('#wrapper').style.opacity = "1"; // !images =======================================================
 
   var articleImg = document.querySelectorAll('.article__photo');
-  articleImg[0].style.backgroundImage = 'url(img/imga.png)';
-  articleImg[1].style.backgroundImage = 'url(img/imgb.png)';
-  articleImg[2].style.backgroundImage = 'url(img/imgc.png)';
-  articleImg[3].style.backgroundImage = 'url(img/imgd.png)';
-  articleImg[4].style.backgroundImage = 'url(img/imge.png)';
-  articleImg[5].style.backgroundImage = 'url(img/imgf.png)';
+
+  function ThisIsWebP() {
+    var img = new Image();
+    img.src = "../img/img1.webp";
+
+    img.onload = function () {
+      return true;
+    };
+
+    img.onerror = function () {
+      return false;
+    };
+  }
+
+  if (ThisIsWebP) {
+    for (var i = 0; i <= articleImg.length - 1; i++) {
+      articleImg[i].style.backgroundImage = 'url(img/img' + i + '.webp)';
+      document.body.style.backgroundImage = 'url(img/bg.webp)';
+    }
+  } else {
+    for (var _i = 0; _i <= articleImg.length - 1; _i++) {
+      articleImg[_i].style.backgroundImage = 'url(img/img' + _i + '.jpg)';
+      document.body.style.backgroundImage = 'url(img/bg.jpg)';
+    }
+  } // !menu ===========================================
+
+
+  document.querySelector('.nav-line').addEventListener('click', MenuClick, false);
+  var isMenuOpen = 0;
+
+  function MenuClick() {
+    if (isMenuOpen == 0) {
+      document.querySelector('.nav-line__nav').style.top = '40px';
+      isMenuOpen = 1;
+    } else if (isMenuOpen == 1) {
+      document.querySelector('.nav-line__nav').style.top = '-500px';
+      isMenuOpen = 0;
+    }
+  }
 };
